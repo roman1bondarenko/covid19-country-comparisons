bb29ff6eb7718a5c8c7945a0efb617c19a8f2921
Roman Bondarenko <romanbondarenko14@gmail.com>
2021-01-10 20:13:47 +0200

    update serving path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/covid19-country-comparisons`));
+app.use(express.static(`${__dirname}/dist/covid-stats-comparation`));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(`${__dirname}/dist/covid19-country-comparisons/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/covid-stats-comparation/index.html`));
 });
 
 app.listen(PORT, () => {
